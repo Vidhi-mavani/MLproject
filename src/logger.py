b5fd679f065c44e4f0f7604828f6c68cb9ab9c0a
Vidhi-mavani <mavanividhi5@gmail.com>
2025-03-23 15:20:55 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -10,10 +10,3 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-# Main code
-if __name__ == "__main__":
-    try:
-        a = 1 / 0  # This will raise a ZeroDivisionError
-    except Exception as e:
-        logging.info("Divide by zero")  # Log the error
-        raise CustomException("A custom error occurred", sys) from e  # Raise the custom exception
